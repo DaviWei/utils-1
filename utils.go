83598c64fa53d10f9d7050243f471a0f9b9c6d8e
Martin Bruse <zondolfin@gmail.com>
2013-11-15 15:05:54 +0100

    added Prettify

---
+++ utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func Prettify(obj interface{}) string {
+	b, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		return fmt.Sprintf("%+v", obj)
+	}
+	return string(b)
+}
