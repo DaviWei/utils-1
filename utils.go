622333473fd7d088feb6818a6c960ec91c6609cd
Martin Bruse <zondolfin@gmail.com>
2013-11-21 11:02:46 +0100

    added JSONContext.LoadJSON, and util.InSlice

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 )
 
 func Prettify(obj interface{}) string {
@@ -11,4 +12,20 @@ func Prettify(obj interface{}) string {
 		return fmt.Sprintf("%+v", obj)
 	}
 	return string(b)
+}
+
+func InSlice(slice interface{}, needle interface{}) bool {
+	sliceValue := reflect.ValueOf(slice)
+	if sliceValue.Kind() != reflect.Slice {
+		panic(fmt.Errorf("%#v is not a slice", slice))
+	}
+	if sliceValue.Type().Elem() != reflect.TypeOf(needle) {
+		panic(fmt.Errorf("%#v is a slice of %#v", slice, needle))
+	}
+	for i := 0; i < sliceValue.Len(); i++ {
+		if reflect.DeepEqual(sliceValue.Index(i).Interface(), needle) {
+			return true
+		}
+	}
+	return false
 }
--- web/jsoncontext/context.go
+++ web/jsoncontext/context.go
@@ -3,15 +3,19 @@ package jsoncontext
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/soundtrackyourbrand/utils"
 	"github.com/soundtrackyourbrand/utils/web/httpcontext"
 	"net/http"
+	"reflect"
 	"strconv"
+	"strings"
 )
 
 type JSONContext interface {
 	httpcontext.HTTPContext
 	APIVersion() int
 	DecodeJSON(i interface{}) error
+	LoadJSON(i interface{}, accessScope string) error
 }
 
 type JSONContextLogger interface {
@@ -40,6 +44,59 @@ func NewJSONContext(c httpcontext.HTTPContextLogger) (result *DefaultJSONContext
 
 func (self DefaultJSONContext) DecodeJSON(i interface{}) error {
 	return json.NewDecoder(self.Req().Body).Decode(i)
+}
+
+func (self DefaultJSONContext) LoadJSON(out interface{}, accessScope string) (err error) {
+
+	var decodedJSON map[string]*json.RawMessage
+	if err = json.NewDecoder(self.Req().Body).Decode(&decodedJSON); err != nil {
+		return
+	}
+
+	structPointerValue := reflect.ValueOf(out)
+	if structPointerValue.Kind() != reflect.Ptr {
+		err = fmt.Errorf("%#v is not a pointer to a struct", out)
+		return
+	}
+	structValue := structPointerValue.Elem()
+	if structValue.Kind() != reflect.Struct {
+		err = fmt.Errorf("%#v is not a pointer to a struct.", out)
+		return
+	}
+
+	structType := structValue.Type()
+	for i := 0; i < structValue.NumField(); i++ {
+		valueField := structValue.Field(i)
+		typeField := structType.Field(i)
+
+		allowedScopes := strings.Split(typeField.Tag.Get("update_scopes"), ",")
+		jsonAttributeName := typeField.Name
+		if jsonTag := typeField.Tag.Get("json"); jsonTag != "" {
+			jsonAttributeName = strings.Split(jsonTag, ",")[0]
+		}
+
+		// Newer try to update field '-'
+		if jsonAttributeName == "-" {
+			continue
+		}
+
+		// Check if a update for this field exist in the source json data.
+		data, found := decodedJSON[jsonAttributeName]
+		if !found {
+			continue
+		}
+
+		// Check that the scope user are in, is allowed to update this field.
+		if !utils.InSlice(allowedScopes, accessScope) {
+			continue
+		}
+
+		// Use json unmarshal the raw value in to correct field.
+		if err = json.Unmarshal(*data, valueField.Addr().Interface()); err != nil {
+			return
+		}
+	}
+	return
 }
 
 func (self DefaultJSONContext) Render(resp Response) error {
