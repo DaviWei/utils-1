c7c367c3a4d282bcb7f0e107a71311c3f3a9ff36
Martin Bruse <zondolfin@gmail.com>
2014-01-21 16:32:26 +0100

    i thought we committed this from svens machine??

--- web/jsoncontext/dochandler.go
+++ web/jsoncontext/dochandler.go
@@ -362,7 +362,7 @@ func CreateResponseFunc(fType reflect.Type, fVal reflect.Value) func(c JSONConte
 		if status := int(results[0].Int()); status != 0 {
 			response.Status = status
 		}
-		if len(results) == 3 && !results[1].IsNil() {
+		if len(results) == 3 && (results[1].Kind() != reflect.Ptr || !results[1].IsNil()) {
 			response.Body = results[1].Interface()
 		}
 		return
