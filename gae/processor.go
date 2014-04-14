1b1f3c58726235edd7086f2f2a5c2760a2750e56
Fredrik Enestad <fredrik.enestad@soundtrackyourbrand.com>
2014-04-14 13:55:51 +0200

    added old entity as parameter for updates and added before delete hook

--- gae/processor.go
+++ gae/processor.go
@@ -9,6 +9,7 @@ const (
 	BeforeCreateName = "BeforeCreate"
 	BeforeUpdateName = "BeforeUpdate"
 	BeforeSaveName   = "BeforeSave"
+	BeforeDeleteName = "BeforeDelete"
 	AfterCreateName  = "AfterCreate"
 	AfterUpdateName  = "AfterUpdate"
 	AfterSaveName    = "AfterSave"
@@ -20,6 +21,7 @@ var processors = []string{
 	BeforeCreateName,
 	BeforeUpdateName,
 	BeforeSaveName,
+	BeforeDeleteName,
 	AfterCreateName,
 	AfterUpdateName,
 	AfterSaveName,
@@ -27,15 +29,20 @@ var processors = []string{
 	AfterDeleteName,
 }
 
-func runProcess(c PersistenceContext, model interface{}, name string) error {
+func runProcess(c PersistenceContext, model interface{}, name string, arg interface{}) error {
 	contextFunc := reflect.ValueOf(c).MethodByName(name).Interface().(func(interface{}) error)
 	if err := contextFunc(model); err != nil {
 		return err
 	}
-	if process, found, err := getProcess(model, name); err != nil {
+	if process, found, err := getProcess(model, name, arg); err != nil {
 		return err
 	} else if found {
-		results := process.Call([]reflect.Value{reflect.ValueOf(c)})
+		var results []reflect.Value
+		if process.Type().NumIn() == 2 {
+			results = process.Call([]reflect.Value{reflect.ValueOf(c), reflect.ValueOf(arg)})
+		} else {
+			results = process.Call([]reflect.Value{reflect.ValueOf(c)})
+		}
 		if len(results) > 1 {
 			if !results[len(results)-1].IsNil() {
 				return results[len(results)-1].Interface().(error)
@@ -45,17 +52,28 @@ func runProcess(c PersistenceContext, model interface{}, name string) error {
 	return nil
 }
 
-func getProcess(model interface{}, name string) (process reflect.Value, found bool, err error) {
+func getProcess(model interface{}, name string, arg interface{}) (process reflect.Value, found bool, err error) {
 	val := reflect.ValueOf(model)
 	if process = val.MethodByName(name); process.IsValid() {
 		processType := process.Type()
-		if processType.NumIn() != 1 {
-			err = fmt.Errorf("%+v#%v doesn't take exactly one argument", model, name)
-			return
-		}
-		if !processType.In(0).Implements(reflect.TypeOf((*PersistenceContext)(nil)).Elem()) {
-			err = fmt.Errorf("%+v#%v takes a %v, not a %v as argument", model, name, processType.In(0))
-			return
+		if processType.NumIn() == 2 {
+			if arg == nil {
+				err = fmt.Errorf("%+v#%v takes two arguments, but we don't have a second argument!", model, name)
+			}
+			if !processType.In(0).Implements(reflect.TypeOf((*PersistenceContext)(nil)).Elem()) {
+				err = fmt.Errorf("%+v#%v takes a %v, not a PersistenceContext as first argument", model, name, processType.In(0))
+				return
+			}
+			if !reflect.TypeOf(arg).AssignableTo(processType.In(1)) {
+				err = fmt.Errorf("%+v#%v takes a %v, not a %v as second argument", model, name, processType.In(0), reflect.TypeOf(arg))
+				return
+			}
+		} else if processType.NumIn() == 1 {
+			if !processType.In(0).Implements(reflect.TypeOf((*PersistenceContext)(nil)).Elem()) {
+				err = fmt.Errorf("%+v#%v takes a %v, not a %v as argument", model, name, processType.In(0))
+				return
+			}
+
 		}
 		if processType.NumOut() != 1 {
 			err = fmt.Errorf("%+v#%v doesn't produce exactly one return value", model, name)
@@ -66,15 +84,6 @@ func getProcess(model interface{}, name string) (process reflect.Value, found bo
 			return
 		}
 		found = true
-	}
-	return
-}
-
-func validateProcessors(model interface{}) (err error) {
-	for _, name := range processors {
-		if _, _, err = getProcess(model, name); err != nil {
-			return
-		}
 	}
 	return
 }
