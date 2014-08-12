9eb9e6c4463925d47be21284fa685717d019520f
Martin Bruse <zondolfin@gmail.com>
2014-08-12 13:58:15 +0200

    more funcs!

--- run/run.go
+++ run/run.go
@@ -18,30 +18,53 @@ func (self StderrError) Error() string {
 	return string(self)
 }
 
+func Start(path string, args ...string) (result chan error) {
+	return startAndLog("", path, args...)
+}
+
 func StartAndLog(logfile string, path string, args ...string) (result chan error) {
+	return startAndLog(logfile, path, args...)
+}
+
+func startAndLog(logfile string, path string, args ...string) (result chan error) {
 	result = make(chan error, 1)
 
-	file, err := os.Create(logfile)
-	if err != nil {
-		result <- err
-		return
+	var file *os.File
+	var err error
+	if logfile != "" {
+		if file, err = os.Create(logfile); err != nil {
+			result <- err
+			return
+		}
 	}
 
 	fmt.Printf(" ( *** %v ) %v", Host, path)
 	for _, bit := range args {
 		fmt.Printf(" %#v", bit)
 	}
-	fmt.Printf(" > %#v\n", logfile)
+	if logfile != "" {
+		fmt.Printf(" > %#v\n", logfile)
+	} else {
+		fmt.Printf("\n")
+	}
 
 	cmd := exec.Command(path, args...)
-	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, file, file
+
+	cmd.Stdin = os.Stdin
+	if logfile != "" {
+		cmd.Stdout, cmd.Stderr = file, file
+	} else {
+		cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	}
 	if err = cmd.Start(); err != nil {
 		result <- err
 		return
 	}
 
 	go func() {
-		defer file.Close()
+		if logfile != "" {
+			defer file.Close()
+		}
 		result <- cmd.Wait()
 	}()
 
