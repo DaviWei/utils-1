2ffa18124df64ccd0f3b6fbce9dd0e1242ee90fd
sfro <sofia@soundtrackyourbrand.com>
2014-08-27 13:29:42 +0200

    Don't want to create a job, we want to create tables.

--- gae/bigquery/bigquery.go
+++ gae/bigquery/bigquery.go
@@ -1,8 +1,12 @@
 package bigquery
 
 import (
-	gbigquery "code.google.com/p/google-api-go-client/bigquery/v2"
+	"fmt"
 	"net/http"
+	"reflect"
+
+	gbigquery "code.google.com/p/google-api-go-client/bigquery/v2"
+	"code.google.com/p/google-api-go-client/googleapi"
 )
 
 const (
@@ -11,18 +15,36 @@ const (
 	datasetId = "test_dataset" //"warehouse"
 )
 
+const (
+	BigqueryScope = gbigquery.BigqueryScope
+)
+
 type BigQuery struct {
-	service *gbigquery.Service
+	service   *gbigquery.Service
+	projectId string
+	datasetId string
 }
 
-func New(client *http.Client) (result *BigQuery, err error) {
+func New(client *http.Client, projectId, datasetId string) (result *BigQuery, err error) {
 	service, err := gbigquery.New(client)
 	if err != nil {
 		return
 	}
 	result = &BigQuery{
-		service: service,
+		service:   service,
+		projectId: projectId,
+		datasetId: datasetId,
 	}
+	return
+}
+
+func (self *BigQuery) createTable(val reflect.Value) (err error) {
+	fmt.Println("Want to create table for", val)
+	return
+}
+
+func (self *BigQuery) patchTable(val reflect.Value, table *gbigquery.Table) (err error) {
+	fmt.Println("Want to patch table for", val, "and", table)
 	return
 }
 
@@ -35,33 +57,18 @@ the latest (counted by UpdatedAt) row per unique Id.
 It assumes that i has a field "Id" that is a key.Key, and a field "UpdatedAt" that is a utils.Time.
 */
 func (self *BigQuery) AssertTable(i interface{}) (err error) {
-	table := &gbigquery.Table{}
-	table.TableReference = &gbigquery.TableReference{
-		DatasetId: datasetId,
-		ProjectId: projectId,
-		TableId:   "test_data",
+	val := reflect.ValueOf(i)
+	for val.Kind() == reflect.Ptr {
+		val = val.Elem()
 	}
-	job := &gbigquery.Job{
-		Configuration: &gbigquery.JobConfiguration{
-			Load: &gbigquery.JobConfigurationLoad{
-				DestinationTable: &gbigquery.TableReference{
-					DatasetId: datasetId,
-					ProjectId: projectId,
-					TableId:   table.TableReference.TableId,
-				},
-				//MaxBadRecords:    source.maxBadRecords,
-				//Schema:           &source.schema,
-				//SourceUris:       []string{source.uri},
-				//WriteDisposition: source.disposition,
-			},
-		},
-	}
-
-	call := self.service.Jobs.Insert(projectId, job)
-	job, err = call.Do()
+	tableService := gbigquery.NewTablesService(self.service)
+	table, err := tableService.Get(self.projectId, self.datasetId, val.Type().Name()).Do()
 	if err != nil {
-		return err
+		if gapiErr, ok := err.(*googleapi.Error); ok && gapiErr.Code == 404 {
+			return self.createTable(val)
+		} else {
+			return
+		}
 	}
-
-	return
+	return self.patchTable(val, table)
 }
