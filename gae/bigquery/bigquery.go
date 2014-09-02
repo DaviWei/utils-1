2c9bf52b79a670f328531cc9c20511454bfc4be6
sfro <sofia@soundtrackyourbrand.com>
2014-09-02 13:26:23 +0200

    First version of inserting data into table.

--- gae/bigquery/bigquery.go
+++ gae/bigquery/bigquery.go
@@ -183,3 +183,137 @@ func (self *BigQuery) AssertTable(i interface{}) (err error) {
 	}
 	return self.patchTable(typ, table)
 }
+
+func (self *BigQuery) InsertTableData(i interface{}) (err error) {
+	request := &gbigquery.TableDataInsertAllRequest{
+		// Kind: The resource type of the response.
+		//Kind string `json:"kind,omitempty"`
+
+		Rows: buildRows(i),
+		//Rows  `json:"rows,omitempty"`
+	}
+
+	tabledataService := gbigquery.NewTabledataService(self.service)
+
+	typ := reflect.TypeOf(i)
+	for typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+
+	tableDataList, err := tabledataService.InsertAll(self.projectId, self.datasetId, typ.Name(), request).Do()
+	if err != nil {
+		return
+	}
+	// Unfound rows are ignored :O
+	for _, errors := range tableDataList.InsertErrors {
+		for _, errorProto := range errors.Errors {
+			fmt.Printf("\nerr:%#v\n", errorProto)
+		}
+	}
+	return
+}
+
+func buildRows(i interface{}) (result []*gbigquery.TableDataInsertAllRequestRows) {
+	val := reflect.ValueOf(i)
+	for val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	typ := reflect.TypeOf(i)
+	for typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		fieldName, fieldData := formatData(field, val.FieldByName(field.Name))
+
+		result = append(result, &gbigquery.TableDataInsertAllRequestRows{
+			// InsertId string `json:"insertId,omitempty"`
+
+			// Json: [Required] A JSON object that contains a row of data. The
+			// object's properties and values must match the destination table's
+			// schema.
+			Json: map[string]gbigquery.JsonValue{fieldName: fieldData}, //map[string]JsonValue `json:"json,omitempty"`
+		},
+		)
+	}
+
+	for _, derp := range result {
+		fmt.Printf("\nrows:%#v\n", derp.Json)
+	}
+	return
+}
+
+func formatData(field reflect.StructField, fieldValue reflect.Value) (fieldName string, fieldData interface{}) {
+	fieldType := field.Type
+	for fieldType.Kind() == reflect.Ptr {
+		fieldType = fieldType.Elem()
+	}
+	switch fieldType.Kind() {
+	case reflect.Bool:
+		return field.Name, fieldValue.Bool()
+
+	case reflect.Float32:
+		fallthrough
+	case reflect.Float64:
+		return field.Name, fieldValue.Float()
+
+	case reflect.String:
+		return field.Name, fieldValue.String()
+
+	case reflect.Uint:
+		fallthrough
+	case reflect.Uint8:
+		fallthrough
+	case reflect.Uint16:
+		fallthrough
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Uint64:
+		return field.Name, fieldValue.Uint()
+
+	case reflect.Int:
+		fallthrough
+	case reflect.Int8:
+		fallthrough
+	case reflect.Int16:
+		fallthrough
+	case reflect.Int32:
+		fallthrough
+	case reflect.Int64:
+		return field.Name, fieldValue.Int()
+
+	case reflect.Struct:
+		return "StringData", "herp"
+		/*switch fieldType {
+			case byteStringType:
+				return field.Name, string(reflect.ValueOf(field).Bytes())
+
+			case timeType:
+				return field.Name, time.Now()
+
+			default:
+				var fieldFields []*gbigquery.TableFieldSchema
+					if fieldFields, err = buildSchemaFields(fieldType); err != nil {
+						return
+					}
+					result = append(result, &gbigquery.TableFieldSchema{
+						Name:   field.Name,
+						Type:   dataTypeRecord,
+						Fields: fieldFields,
+					})
+			}
+
+		/*case reflect.Slice:
+		// Assume that slices are byte slices and base64 encoded
+		result = append(result, &gbigquery.TableFieldSchema{
+			Name: field.Name,
+			Type: dataTypeString,
+		})*/
+	default:
+		return "StringData", "herp"
+		//err = utils.Errorf("Unsupported kind for schema field: %v", field)
+		return
+	}
+
+	return
+}
