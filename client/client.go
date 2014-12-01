d4ed42a7868d8167b10326a11df7396d335c671e
Fredrik Enestad <fredrik.enestad@soundtrackyourbrand.com>
2014-12-01 17:39:55 +0100

    added method for fetching spotify accounts

--- client/client.go
+++ client/client.go
@@ -239,6 +239,8 @@ type RemoteSlot struct {
 	Schedule key.Key `json:"-"`
 }
 
+type RemoteSpotifyAccounts []RemoteSpotifyAccount
+
 type RemoteSpotifyAccount struct {
 	DefaultMeta
 	SoundZone          key.Key    `json:"sound_zone" datastore:"-"`
@@ -712,6 +714,21 @@ func GetSpotifyAccount(c ServiceConnector, soundZone key.Key, token AccessToken)
 
 	result = &RemoteSpotifyAccount{}
 	err = json.NewDecoder(response.Body).Decode(result)
+	return
+}
+
+func GetSpotifyAccounts(c ServiceConnector, account key.Key, token AccessToken) (result RemoteSpotifyAccounts, err error) {
+	request, response, err := DoRequest(c, "GET", c.GetAuthService(), fmt.Sprintf("accounts/%v/payment_method/spotify_accounts", account.Encode()), token, nil)
+	if err != nil {
+		return
+	}
+	if response.StatusCode != 200 {
+		err = errorFor(request, response)
+		return
+	}
+
+	result = RemoteSpotifyAccounts{}
+	err = json.NewDecoder(response.Body).Decode(&result)
 	return
 }
 
